1955bf78ff385dab6550d78ec91729b1c9a70894
Lzeey <zeyi@berkeley.edu>
2018-03-22 20:44:17 +0800

    change theme in bash-it to powerline-plain

--- bash/bashit.sh
+++ bash/bashit.sh
@@ -1,2 +1,5 @@
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-~/.bash_it/install.sh --no-modify-config
+#Runs the installation script. Requires a yes prompt
+~/.bash_it/install.sh
+#Replaces default bobby theme with powerline-plain
+sed -i 's/BASH_IT_THEME=.*/BASH_IT_THEME="powerline-plain"/' ~/.bashrc
