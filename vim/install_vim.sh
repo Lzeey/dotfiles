1d3168bc8bad606713944c225fa9dba7882944ae
lzeey <lzeey@gmail.com>
2018-03-30 11:41:28 +0800

    Downloads colorscheme

--- vim/install_vim.sh
+++ vim/install_vim.sh
@@ -8,6 +8,12 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # Get NERDTree
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
+# Install colorschemes
+git clone https://github.com/flazz/vim-colorschemes.git ~/.vim/colorschemes
+mv ~/.vim/colorschemes/* ~/.vim/
+rm -rf ~/.vim/colorschemes/
+rm ~/.vim/README.md
+
 #To install color schemes and apply standardized config
 cp .vimrc ~/.vimrc
 cp -r .vim ~/.vim
