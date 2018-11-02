bb9bbcc2f8340bf8185b1948981abab430f9145a
admin <admin@admins-MacBook-Pro.local>
2018-11-02 16:15:01 +0800

    iterm2

---
+++ iterm2/mac_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+brew cask install iterm2
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
